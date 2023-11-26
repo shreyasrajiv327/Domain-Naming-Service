199ace4a37533c716b380112a11f187b092de82f
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2023-11-26 23:03:38 +0530

    done

--- React/src/App.js
+++ React/src/App.js
@@ -340,7 +340,7 @@ const App = () => {
 				<div className="header-container">
 					<header>
 						<div className="left">
-							<p className="title">CryptoConnect Naming Service💰🤑</p>
+							<p className="title">Shreyas's Naming Service</p>
 							<p className="subtitle">Your immortal API on the blockchain!</p>
 						</div>
 						{/* Display a logo and wallet connection status*/}
